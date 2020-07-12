8d9282a37195293dff959b1af2281d62dbac168a
Your Name <you@example.com>
2020-07-13 00:47:44 +0530

    facebook fish settings debug

--- justfish/settings.py
+++ justfish/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5h9$+n84xol+a-st%hhn545$y%%f8son3cadi$#r66k*fy)w=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
